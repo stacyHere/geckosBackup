7d8652fa0b600a17908865db3f0e53f3bacd71c1
stacy <stacykillinger@gmail.com>
2018-06-18 15:47:42 -0400

    add comments and audio files

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -3,19 +3,20 @@ const btn5 = document.getElementById('btn5');
 const btn7 = document.getElementById('btn7');
 const timer = document.querySelector('.timer');
 const startBtn = document.querySelector('.startBtn');
-const audio3 = new Audio ("working_tools/track1 _meditate - Mindful Breathing Meditation - 3min.mp3");
-const audio5 = new Audio ("working_tools/Track2  - Mindful Breathing Meditation - 5min.mp3");
-const audio7 = new Audio ("working_tools/Track 3 - Meditation for Spiritual Awakening  7 min .mp3");
+
 let duration;
 let diffTime;
 let timeCount;
 let time;
+let myAudio;
+
+//start is hidden until a time is chosen
 startBtn.disabled = true;
 startBtn.style.visibility = 'hidden';
 
 function update(t) {
   time = t;
-  timer.innerHTML = t + ":00";
+  timer.innerHTML = t + ":00";  //display before starting
   btn3.disabled = true;
   btn5.disabled = true;
   btn7.disabled = true;
@@ -31,7 +32,14 @@ function meditateNow() {
   duration = moment.duration(duration.asMilliseconds() - 1000, 'milliseconds');
   let min = moment.duration(duration).minutes();
   let sec = moment.duration(duration).seconds();
+  //if less than 9 sec left pad with a zero in front
+  if(sec <= 9){
+    timer.innerHTML = min + ":0" + sec;
+  //  sec.padStart(0);
+}else{
   timer.innerHTML = min + ":" + sec;
+}
+  //when timer is done
   if(sec <= 0 && min <= 0) {
     clearInterval(timeCount);
     timer.innerHTML = "You have completed " + time + " minutes of meditation";
@@ -39,8 +47,17 @@ function meditateNow() {
 }
 
 function startInterval() {
+  if(time == 3){
+    myAudio = new Audio ("working_tools/track1.mp3");
+  }
+else if(time == 5) {
+    myAudio = new Audio ("working_tools/track2.mp3");
+  }
+  else if(time == 7) {
+    myAudio = new Audio ("working_tools/track3.mp3");
+  }
+  myAudio.play();
   if(diffTime > 0) {
-    audio3.play();
     timeCount = setInterval(meditateNow, 1000);
   }
 }
